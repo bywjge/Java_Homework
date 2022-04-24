74b18d5470a8506029aa7015eec3884432a486c8
OXL <oxl02@qq.com>
2022-04-24 21:38:48 +0800

    20220424

---
+++ src/ChapterThree/Exercise5_1.java
@@ -0,0 +1,8 @@
+package ChapterThree;
+
+public class Exercise5_1 {
+    public static void main(String[] args) {
+        int startPosition = 0, endPosition = 0;
+        char cStart = 'a', cEnd = 'b';
+    }
+}
